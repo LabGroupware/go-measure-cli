723605dbcfe720f6a4e02606d4a1758d5ab0b79c
omniarchy <k.hayashi@cresplanex.com>
2024-12-09 21:23:13 +0900

    mv

--- move_matching.sh
+++ move_matching.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # 使用法を表示
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <source_directory> <line_count> <target_directory>"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <source_directory>" "<base_output_dir>"
   exit 1
 fi
 
 # 引数を変数に格納
 source_directory=$1
-line_count=$2
-target_directory=$3
 
 # ソースディレクトリが存在するか確認
 if [ ! -d "$source_directory" ]; then
@@ -17,23 +15,51 @@ if [ ! -d "$source_directory" ]; then
   exit 1
 fi
 
-# ターゲットディレクトリが存在するか確認し、なければ作成
-if [ ! -d "$target_directory" ]; then
-  mkdir -p "$target_directory"
-fi
+# ディレクトリ構造の定義と行数マッピング
+base_output_dir=$2
+declare -A categories=(
+  ["team"]=5
+  ["file_object"]=4
+  ["organization"]=6
+  ["task"]=7
+  ["user_profile"]=4
+)
 
-# 指定した行数と一致するファイルを移動
-find "$source_directory" -type f | while read -r file; do
-  actual_lines=$(wc -l < "$file")
-  if [ "$actual_lines" -eq "$line_count" ]; then
-    # 現在のタイムスタンプを取得
-    timestamp=$(date "+%Y%m%d_%H%M%S")
-    # 元のファイル名を取得
-    filename=$(basename "$file")
-    # 新しいファイル名を生成
-    new_filename="${timestamp}_${filename}"
-    # ファイルを移動しつつ名前を変更
-    mv "$file" "$target_directory/$new_filename"
-    # echo "Moved: $file to $target_directory/$new_filename"
+# ネストされたすべてのファイルを処理
+find "$source_directory" -type f -name "socket_subscribe_output_create_*_*.csv" | while read -r file; do
+  # 現在のタイムスタンプを取得
+  timestamp=$(date "+%Y%m%d_%H%M%S")
+
+  # ファイル名からカテゴリを抽出
+  filename=$(basename "$file")
+  echo $filename
+  category=$(echo "$filename" | sed -n 's/^socket_subscribe_output_create_\([a-z_]*\)_.*\.csv$/\1/p')
+  echo $category
+
+  # カテゴリが定義されている場合のみ処理
+  if [[ -n "$category" && -n "${categories[$category]}" ]]; then
+    required_lines=${categories[$category]}
+    target_dir="$base_output_dir/create_$category"
+
+    # 行数を取得
+    actual_lines=$(wc -l < "$file")
+
+    # 行数が一致する場合のみ移動
+    if [ "$actual_lines" -eq "$required_lines" ]; then
+      # ターゲットディレクトリが存在しない場合は作成
+      mkdir -p "$target_dir"
+
+      # 新しいファイル名を生成
+      new_filename="${timestamp}_${filename}"
+
+      # ファイルを移動
+      mv "$file" "$target_dir/$new_filename"
+      echo "Moved: $file -> $target_dir/$new_filename"
+    else
+      echo "Skipped: $file (lines: $actual_lines, required: $required_lines)"
+    fi
+  else
+    echo $category
+    echo "Skipped: $file (unknown category)"
   fi
 done
---
+++ saga_move.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./move_matching.sh "./bench" "./datasets/wait_saga/thread_100"
+./move_matching.sh "./bench" "./datasets/wait_saga/thread_10"
+./move_matching.sh "./bench" "./datasets/wait_saga/thread_1"
+# "./datasets/wait_saga/thread_100"
